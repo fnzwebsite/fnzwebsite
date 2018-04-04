5ba276b2bbe871ea3aa895966bb99ba06921aec0
Koppa Narayana Karthik <karthik.koppanarayana@nagra.com>
2018-04-04 16:50:27 +0530

    fixed broken merge

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,14 +10,13 @@ import * as dealingActions from '../actions/dealingActions';
 import userActions from '../actions/user.actions';
 import * as priceActions from '../actions/priceActions';
 //import {authHeader} from '../helpers';
-import { authHeader,getConfig } from '../helpers';
+import {authHeader, getConfig} from '../helpers';
 import {bindActionCreators} from 'redux';
 import PropTypes from 'prop-types';
 import moment from 'moment'
 import { Redirect} from 'react-router-dom';
 
 class Home extends React.Component {
-<<<<<<< HEAD
     constructor(props) {
         super(props);
         this.state = {
@@ -54,8 +53,10 @@ class Home extends React.Component {
 
     componentDidMount(prevProps, prevState) {
         var self = this;
-        var socket = io('http://localhost:3700', {query: "auth=" + authHeader()['Authorization']});
+//var socket = io('http://localhost:3700', {query: "auth=" + authHeader()['Authorization']});
+        var socket = io(getConfig('socketurl'), {query: "auth=" + authHeader()['Authorization']});
         socket.on('dealing', function (dealing) {
+
             if (self.state.dealing !== dealing) {
                 self.getPrice();
                 self.setState({dealing: dealing});
@@ -75,7 +76,7 @@ class Home extends React.Component {
 
     render() {
         if (this.props.data.dealing == "logout" || this.state.dealing == "logout") {
-            return  <Redirect to={{ pathname: '/login', state: { from: this.props.location } }} />
+            return <Redirect to={{pathname: '/login', state: {from: this.props.location}}}/>
         }
         else {
             var dealing = this.state.dealing || this.props.data.dealing;
@@ -103,13 +104,13 @@ class Home extends React.Component {
                                                                onClick={() => this.changeView('chart')}><i
                                                                 className="fa fa-area-chart"></i>Chart</a></li>
                                                         <li className={this.state.selected == "table" ? 'active' : ''}>
-                                                            <a className={this.state.selected == "chart" ? 'active' : ''}
+                                                            <a className={this.state.selected == "table" ? 'active' : ''}
                                                                onClick={() => this.changeView('table')}><i
                                                                 className="fa fa-th-list"></i>Transactions</a></li>
                                                     </ul>
                                                     <div className="tab-content">
                                                         <div id="chart"
-                                                             className={this.state.selected == "chart" ? 'tab-pane in active' : 'tab-pane fade'}>
+                                                             className={this.state.selected == "chart" ? 'tab-pane in active' : 'tab-pane in fade'}>
                                                             <div className="line-chart-example card">
                                                                 <div className="line-chart-example card">
                                                                     <LoadLineChart loadThisDay={this.state.chart}
@@ -118,197 +119,66 @@ class Home extends React.Component {
                                                             </div>
                                                         </div>
                                                         <div id="table"
-                                                             className={this.state.selected == "table" ? 'tab-pane in active' : 'tab-pane fade'}>
-                                                            <div className="table-responsive">
-                                                                <TransactionsTable loadThisDay={this.state.chart}
-                                                                                   dealingData={dealing}/>
+                                                             className={this.state.selected == "table" ? 'tab-pane in active' : 'tab-pane in  fade'}>
+                                                            <div className="card table-responsive">
+                                                                <div className="card-body">
+                                                                    <TransactionsTable loadThisDay={this.state.chart}
+                                                                                       dealingData={dealing}/>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
 
 
-
-                                        </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                                <footer className="main-footer">
-                                    <div className="container-fluid">
-                                        <div className="row">
-                                            <div className="col-sm-12 text-center">
-                                                <p>&copy; Copyright FNZ UK Ltd 2018.</p>
-                                            </div>
+                            <footer className="main-footer">
+                                <div className="container-fluid">
+                                    <div className="row">
+                                        <div className="col-sm-12 text-center">
+                                            <p>&copy; Copyright FNZ UK Ltd 2018.</p>
                                         </div>
                                     </div>
-                                </footer>
-=======
-constructor(props) {
-super(props);
-this.state = {
-dealing: null,
-chart: 'today',
-selected: 'chart'
-};
-this.changeView = this.changeView.bind(this);
-this.loadChart = this.loadChart.bind(this);
-this.getPrice = this.getPrice.bind(this);
-}
-
-changeView(selected) {
-if (this.state.selected != selected) {
-this.setState({
-selected: selected
-});
-}
-}
-
-loadChart(selected) {
-if (this.state.chart != selected) {
-this.setState({
-chart: selected
-})
-}
-}
-
-componentWillMount(prevProps, prevState) {
-this.props.dealingActions.getDealings();
-this.getPrice();
-}
-
-
-componentDidMount(prevProps, prevState) {
-var self = this;
-//var socket = io('http://localhost:3700', {query: "auth=" + authHeader()['Authorization']});
-        var socket = io(getConfig('socketurl'),{ query: "auth="+authHeader()['Authorization']});
-socket.on('dealing', function (dealing) {
-
-if (self.state.dealing !== dealing) {
-self.getPrice();
-self.setState({dealing: dealing});
-}
-})
-}
-
-getPrice() {
-var today = moment().add('days', 2).format("YYYY-MM-DD");
-var tomorrow = moment().add('days', 1).format("YYYY-MM-DD");
-var yesterday = moment().add('days', -1).format("YYYY-MM-DD");
-
-this.props.priceActions.getPriceKeyDate(today, 'today');
-this.props.priceActions.getPriceKeyDate(tomorrow, 'next');
-this.props.priceActions.getPriceKeyDate(yesterday, 'previous');
-}
-
-render() {
-if (this.props.data.dealing == "logout" || this.state.dealing == "logout") {
-this.props.userActions.logout();
-//            this.history.pushState(null, 'login');
-}
-else {
-var dealing = this.state.dealing || this.props.data.dealing;
-return (
-<div className="page">
-<div className="page-content d-flex align-items-stretch">
-<SideNav/>
-<div className="content-inner">
-<Header/>
-<div className="container-fluid">
-<div className="mt-70">
-
-<BoxToday loadChart={this.loadChart} dealingData={dealing} price={this.props.data}/>
-
-<div className="tab-content">
-    <div id="home" className="tab-pane in active">
-
-        <div className="col-sm-12 chart-sec">
-            <div className="row">
-                <h5>Trades <span></span></h5>
-
-                <ul className="nav nav-tabs chart-tabs has-shadow">
-                    <li className={this.state.selected == "chart" ? 'active' : ''}>
-                        <a className={this.state.selected == "chart" ? 'active' : ''}
-                           onClick={() => this.changeView('chart')}><i
-                            className="fa fa-area-chart"></i>Chart</a></li>
-                    <li className={this.state.selected == "table" ? 'active' : ''}>
-                        <a className={this.state.selected == "table" ? 'active' : ''}
-                           onClick={() => this.changeView('table')}><i
-                            className="fa fa-th-list"></i>Transactions</a></li>
-                </ul>
-                <div className="tab-content">
-                    <div id="chart"
-                         className={this.state.selected == "chart" ? 'tab-pane in active' : 'tab-pane in fade'}>
-                        <div className="line-chart-example card">
-                            <div className="line-chart-example card">
-                                <LoadLineChart loadThisDay={this.state.chart}
-                                               dealingData={dealing}/>
->>>>>>> 76131c9bcccac8c75652787468d8910e0479a52f
-                            </div>
-                        </div>
-                    </div>
-                    <div id="table"
-                         className={this.state.selected == "table" ? 'tab-pane in active' : 'tab-pane in  fade'}>
-                        <div className="card table-responsive">
-                        <div className="card-body">
-                            <TransactionsTable loadThisDay={this.state.chart}
-                                               dealingData={dealing}/>
-                        </div>
+                                </div>
+                            </footer>
                         </div>
                     </div>
                 </div>
-
-
-
-    </div>
-        </div>
-    </div>
-</div>
-</div>
-</div>
-<footer className="main-footer">
-<div className="container-fluid">
-    <div className="row">
-        <div className="col-sm-12 text-center">
-            <p>&copy; Copyright FNZ UK Ltd 2018.</p>
-        </div>
-    </div>
-</div>
-</footer>
-</div>
-</div>
-</div>
-)
-}
-}
+            )
+        }
+    }
 }
 
 const
-mapStateToProps = (state, props) => {
-return {
-data: state,
-user: state.user,
-price:state.priceToday,
-price:state.priceNext,
-price:state.pricePrevious
-}
-};
+    mapStateToProps = (state, props) => {
+        return {
+            data: state,
+            user: state.user,
+            price: state.priceToday,
+            price: state.priceNext,
+            price: state.pricePrevious
+        }
+    };
 
 Home.propTypes = {
-userActions: PropTypes.object,
-user: PropTypes.array,
-priceToday: PropTypes.array,
-priceNext: PropTypes.array,
-pricePrevious: PropTypes.array
+    userActions: PropTypes.object,
+    user: PropTypes.array,
+    priceToday: PropTypes.array,
+    priceNext: PropTypes.array,
+    pricePrevious: PropTypes.array
 };
 
 const
-mapDispatchToProps = (dispatch) => ({
-dealingActions: bindActionCreators(dealingActions, dispatch),
-userActions:bindActionCreators(userActions, dispatch),
-priceActions:bindActionCreators(priceActions, dispatch)
-});
+    mapDispatchToProps = (dispatch) => ({
+        dealingActions: bindActionCreators(dealingActions, dispatch),
+        userActions: bindActionCreators(userActions, dispatch),
+        priceActions: bindActionCreators(priceActions, dispatch)
+    });
 
 
 export default connect(mapStateToProps,
-mapDispatchToProps)(Home);
+    mapDispatchToProps)(Home);
